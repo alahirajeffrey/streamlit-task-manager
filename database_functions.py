df216eeb7f02fb8a62d5111a53e7db543aa786d9
alahirajeffrey <alahirajeffrey@gmail.com>
2023-12-27 20:13:14 +0100

    func to view tasks, single task and delete task

--- database_functions.py
+++ database_functions.py
@@ -23,3 +23,37 @@ def add_subbtask(title, detail, task_id, urgency=1):
     )
     connection.commit()
     cursor.close()
+
+def view_all_tasks():
+    connection = create_connection()
+    cursor = connection.cursor()
+    cursor.execute(
+        """
+        "SELECT * FROM tasks"
+        """
+    )
+    connection.commit()
+    cursor.close()
+
+def view_single_task(task_id):
+    connection = create_connection()
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM tasks WHERE id = ?", (task_id,))
+    task = cursor.fetchone()
+    
+    if task:
+        cursor.execute("SELECT * FROM subtasks WHERE task_id = ?", (task_id,))
+        subtasks = cursor.fetchone()
+        task_with_subtasks = {'task': task, 'subtasks': subtasks}
+    else:
+        return 
+
+    cursor.close()
+    return task_with_subtasks
+
+def delete_task(task_id):
+    connection = create_connection()
+    cursor = connection.cursor()
+    cursor.execute("DELETE * FROM tasks WHERE id = ?", (task_id,))
+    cursor.close()
+    return 
