c2d4f69c59354ad365a475279c2cb8198831a57c
alahirajeffrey <alahirajeffrey@gmail.com>
2023-12-27 20:01:00 +0100

    added functions to add task and subtask

--- database_functions.py
+++ database_functions.py
@@ -1,1 +1,25 @@
 from database import create_connection
+
+def create_task(title, detail, urgency=1):
+    connection = create_connection()
+    cursor = connection.cursor()
+    cursor.execute(
+        """
+        INSERT INTO tasks (title, detail, urgency) VALUES (?, ?, ?)
+        """,
+        (title, detail, urgency)
+    )
+    connection.commit()
+    cursor.close()
+
+def add_subbtask(title, detail, task_id, urgency=1):
+    connection = create_connection()
+    cursor = connection.cursor()
+    cursor.execute(
+        """
+        INSERT INTO subtasks (title, detail, urgency, task_id) VALUES (?, ?, ?, ?)
+        """,
+        (title, detail, urgency, task_id)
+    )
+    connection.commit()
+    cursor.close()
